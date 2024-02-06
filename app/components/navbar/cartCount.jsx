54040942d7369665a6d83778516944b7a150e12e
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2024-02-06 18:33:41 +0530

    dwrew

--- app/components/navbar/cartCount.jsx
+++ app/components/navbar/cartCount.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-export default function cartCount() {
+export default function CartCount() {
     const cartItems = useSelector((store)=>store.cart)
   return (
     <span className='relative bottom-[12px] right-[14px] bg-red-300 rounded-full w-[18px] h-[18px] text-[12px]'>{cartItems.length}</span>
